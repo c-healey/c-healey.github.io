1b0f37ea63db2228567d08d9da83c5d62f9adc8c
Cathy <cathy.healey@gmail.com>
2020-06-10 00:47:59 -0400

    added docs folder and copied contents of build to test if react app can be served out of docs dir

---
+++ docs/precache-manifest.f9f008ff43c1da46be2c9145c5069bf8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93ce385947844f852bbaf89422b94065",
+    "url": "/index.html"
+  },
+  {
+    "revision": "318e0a86f45d0e06b0a3",
+    "url": "/static/css/main.a4d066a0.chunk.css"
+  },
+  {
+    "revision": "576171ca49300b8042a3",
+    "url": "/static/js/2.31b189c0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.31b189c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "318e0a86f45d0e06b0a3",
+    "url": "/static/js/main.b60654d2.chunk.js"
+  },
+  {
+    "revision": "4ed10572a274e3c7e781",
+    "url": "/static/js/runtime-main.21367f1e.js"
+  }
+]);
